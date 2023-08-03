30bb4a3336a0a3c81a0de09206d3a6f3ee06f872
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2023-08-03 16:42:18 +0200

    ilp version finished, but slow

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # payback
 
+This is the ILP version, which is pretty slow.
+
 ## Requirements
 [SCIP](https://scipopt.org/#scipoptsuite)
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,5 @@ pub mod ilp;
 fn main() {
     env_logger::init();
     let graph = Graph::from(vec![("A".to_string(), 1), ("B".to_string(), 2), ("C".to_string(), 3)]);
-    // let graph = Graph::from(vec![("A".to_string(), 1), ("B".to_string(), 3)]);
     ProblemInstance::from(graph).run();
 }
